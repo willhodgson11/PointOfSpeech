ac197f5057d679a357e964ed21bd98b875741749
willhodgson <willhodgson11@gmail.com>
2023-02-22 18:10:57 -0500

    working on viterbi

--- PointOfSpeech.java
+++ PointOfSpeech.java
@@ -1,9 +1,24 @@
+import org.opencv.core.Point;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 
 public class PointOfSpeech {
 
+    public HashMap<String, Integer> observations;               // Maps a given word with
+    public HashMap<String, Map<String, Double>> transitions;    // Maps a given state to all its possible nextStates, with the appropriate score
+    public String trainSentences;
+    public String trainTags;
+
+    /**
+     * class to hold the score and previous type of given state
+     */
     public class State{
 
         public double score;
@@ -21,7 +36,9 @@ public class PointOfSpeech {
     /**
      *  constructor
      */
-    public PointOfSpeech(){
+    public PointOfSpeech(String trainSentencesFile, String trainTagsFile){
+        this.trainSentences = trainSentencesFile;
+        this.trainTags = trainTagsFile;
 
     }
 
@@ -29,11 +46,28 @@ public class PointOfSpeech {
      * perform Viterbi decoding to find the best sequence of tags for a line (sequence of words)
      */
     public String[] viterbi(String[] sentence){
+        //yes
         HashMap<String, Double> currScore = new HashMap<>();
-        ArrayList<String> nextStates = new ArrayList<>();
+        //yes
+        HashSet<String> currStates = new HashSet<>();
+        //idk
+        HashMap<String, State> temp = new HashMap<>();
+        //yes
         ArrayList<Map<String, State>> backPointer = new ArrayList<>();
 
-        currScore.put("#", 0.0);
+        currScore.put("#", 0.0);    // Let  "#" be the tag "before" the start of the sentence.
+
+        for(String currentWord : sentence){
+
+            for(String state : currStates){
+                // For each possible transition associated with the current state
+                for(String transition : transitions.get(state).keySet()){
+
+                }
+
+            }
+        }
+
 
 
     }
@@ -42,18 +76,26 @@ public class PointOfSpeech {
      * trains a model (observation and transition probabilities) on corresponding lines
      * (sentence and tags) from a pair of training files.
      */
-    public void trainModel{
-
+    public void trainModel() throws IOException {
+        BufferedReader sentence = new BufferedReader(new FileReader(trainSentences));
+        BufferedReader tags = new BufferedReader(new FileReader(trainTags));
     }
 
     /**
      * console-based test method to give the tags from an input line.
      */
-    public void testUserInput(){}
+    public void testUserInput(){
+
+    }
 
     /**
      * file-based test method to evaluate the performance on a pair of test files
      */
-    public void testFile(){}
+    public void testFile(){
 
+    }
+
+    public static void main(String[] args) {
+        PointOfSpeech test0 = new PointOfSpeech("simple-train-sentences.txt", "simple-train-tags.txt")
+    }
 }
